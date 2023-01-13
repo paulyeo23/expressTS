2574a54f5da6256bbcd7d9eafb29884082b646b0
Paul mavericks <paulmavericks@Maverickss-MacBook-Air.local>
2023-01-13 16:15:19 +0800

    added pg config

---
+++ dist/config/config.js
@@ -0,0 +1,11 @@
+"use strict";
+module.exports = {
+    development: {
+        username: "postgres",
+        password: "123456ty",
+        database: "employees",
+        host: "127.0.0.1",
+        dialect: "postgres",
+        // query: { raw: true },
+    },
+};
