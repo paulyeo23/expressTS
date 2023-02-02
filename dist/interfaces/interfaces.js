77fb935f7df2e70c58cb9858de32afb8080639e0
paulyeo23 <78999327+paulyeo23@users.noreply.github.com>
2023-02-02 12:26:57 +0800

    Delete dist directory

--- dist/interfaces/interfaces.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.departmentEnum = void 0;
-var departmentEnum;
-(function (departmentEnum) {
-    departmentEnum["HR"] = "HR";
-    departmentEnum["PS"] = "PS";
-})(departmentEnum = exports.departmentEnum || (exports.departmentEnum = {}));
